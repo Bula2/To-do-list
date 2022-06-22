25014d1e6dacef4ef34d874c9422946b6f889d0c
Dmitrii Bulaev <71011407+Bula2@users.noreply.github.com>
2022-06-22 23:28:06 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -46,8 +46,8 @@ function addItem(){
 
 function changeBack(){
     let back = document.body;
-    if (back.style.backgroundImage !== "url(\"../img/2.jpg\")")
-        back.style.backgroundImage = "url(\"../img/2.jpg\")";
+    if (back.style.backgroundImage !== 'url("../img/2.jpg")')
+        back.style.backgroundImage = 'url("../img/2.jpg")';
     else
         back.style.backgroundImage = "linear-gradient(to right, #dc1a98, #01b7d7, #348cd5)";
 }
